c029f4cae523792dc0d46d1c6f981f465a03e5d3
Kooreis <kooreis.official@gmail.com>
2024-08-13 12:43:26 -0500

    Defined the function is_rotation with two string parameters s1 and s2. This function will check if one string is a rotation of the other.

--- solution.py
+++ solution.py
@@ -1,15 +1,1 @@
 def is_rotation(s1: str, s2: str) -> bool:
-    if len(s1) != len(s2):
-        return False
-    return s1 in s2 + s2
-
-def main():
-    s1 = input("Enter the first string: ")
-    s2 = input("Enter the second string: ")
-    if is_rotation(s1, s2):
-        print(f"{s2} is a rotation of {s1}")
-    else:
-        print(f"{s2} is not a rotation of {s1}")
-
-if __name__ == "__main__":
-    main()
