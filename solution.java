b065b1a8453be0feb4ecda531ac58db2700b38ea
Kooreis <kooreis.official@gmail.com>
2024-08-13 12:43:26 -0500

    In the first update, a Scanner object is created to take user input. The user is prompted to enter two strings, which are then stored in the variables s1 and s2.

--- solution.java
+++ solution.java
@@ -1,6 +1,3 @@
-Here is a Java console application that solves the problem:
-
-```java
 import java.util.Scanner;
 
 public class Main {
@@ -11,22 +8,5 @@ public class Main {
         System.out.println("Enter the second string:");
         String s2 = scanner.nextLine();
         scanner.close();
-
-        if (isRotation(s1, s2)) {
-            System.out.println("The second string is a rotation of the first string.");
-        } else {
-            System.out.println("The second string is not a rotation of the first string.");
-        }
-    }
-
-    private static boolean isRotation(String s1, String s2) {
-        if (s1.length() != s2.length()) {
-            return false;
-        }
-        String concatenated = s1 + s1;
-        return concatenated.contains(s2);
     }
 }
-```
-
-This program first takes two strings as input from the user. It then checks if the second string is a rotation of the first string by concatenating the first string to itself and checking if the second string is a substring of the result. If the second string is a substring, it is a rotation of the first string.
