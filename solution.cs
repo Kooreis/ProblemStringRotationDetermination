20fcb5628f9f8d1f65ce48c7bfb54095300211ee
Kooreis <kooreis.official@gmail.com>
2024-08-14 11:43:26 -0500

    In the second update, a conditional statement is added to the main function. This statement calls the IsRotation function (which is not yet defined) with the two user-inputted strings as arguments. Depending on the return value of IsRotation, a message is printed to the console stating whether or not the second string is a rotation of the first.

--- solution.cs
+++ solution.cs
@@ -9,5 +9,10 @@ class Program
 
         Console.Write("Enter second string: ");
         string str2 = Console.ReadLine();
+
+        if (IsRotation(str1, str2))
+            Console.WriteLine("The second string is a rotation of the first string.");
+        else
+            Console.WriteLine("The second string is not a rotation of the first string.");
     }
 }
